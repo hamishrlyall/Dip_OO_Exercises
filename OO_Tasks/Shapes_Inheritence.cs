71670a482a9e6ae2a0759dc147bef692a2061b66
HAMISH LYALL <101614208@student.swin.edu.au>
2018-04-21 15:21:02 +1000

    Updated Code Error Free Untested

--- OO_Tasks/Shapes_Inheritence.cs
+++ OO_Tasks/Shapes_Inheritence.cs
@@ -18,7 +18,7 @@ namespace OO_Tasks_Inheritence {
             return _name;
         }
 
-        public virtual Shape(string Name, string Colour)
+        public Shape(string Name, string Colour)
         {
 
         }
@@ -40,7 +40,7 @@ namespace OO_Tasks_Inheritence {
     {
         private int _NumSides = 4;
 
-        public virtual Quadrilateral(string Name, string Colour)
+        public Quadrilateral(string Name, string Colour) : base (Name, Colour)
         {
 
         }
@@ -55,7 +55,7 @@ namespace OO_Tasks_Inheritence {
     {
         private double _sideLength;
 
-        public Square (string Name, string Colour, string SideLength)
+        public Square (string Name, string Colour, double SideLength) : base (Name, Colour)
         {
 
         }
@@ -70,7 +70,7 @@ namespace OO_Tasks_Inheritence {
             return _sideLength * _sideLength;
         }
 
-        public double GetPerimeter()
+        public override double GetPerimeter()
         {
             return 4 * _sideLength;
         }
@@ -82,7 +82,7 @@ namespace OO_Tasks_Inheritence {
         private double _length;
         private double _width;
 
-        public Rectangle (string Name, string Colour, double Length, double Width)
+        public Rectangle (string Name, string Colour, double Length, double Width) : base(Name, Colour)
         {
 
         }
@@ -114,7 +114,7 @@ namespace OO_Tasks_Inheritence {
         public const double PI = 3.142;
         private double _radius;
 
-        public Circle(string Name, string Colour, string Radius)
+        public Circle(string Name, string Colour, double Radius) : base(Name, Colour)
         {
 
         }
@@ -124,7 +124,7 @@ namespace OO_Tasks_Inheritence {
             return _radius;
         }
 
-        public double GetArea()
+        public override double GetArea()
         {
             return _radius * _radius * PI;
         }
@@ -134,7 +134,7 @@ namespace OO_Tasks_Inheritence {
             return 2 * _radius * PI;
         }
 
-        public double GetPerimeter()
+        public override double GetPerimeter()
         {
             return 2 * _radius * PI;
         }
--- OO_Tasks/Shapes_Interface.cs
+++ OO_Tasks/Shapes_Interface.cs
@@ -2,25 +2,141 @@
 
 namespace OO_Tasks_Interface {
 
-    public interface IShapeProps { }
+    public interface IShapeProps
+    {
+        /*public double GetArea()
+        {
+            return 0;
+        }
 
-    public interface ICircleProps { }
+        public double GetPerimeter()
+        {
+            return 0;
+        }*/
+    }
 
-    public class Shape {
+    public interface ICircleProps
+    {
+        /*public double GetArea()
+        {
+            return 0;
+        }
+
+        public double GetCircumference()
+        {
+            return 0;
+        }*/
+    }
+
+    public class Shape
+    {
 
         private string _name;
 
         private string _colour;
+
+        public Shape(string Name, string Colour)
+        {
+
+        }
+
+        public string Colour()
+        {
+            return _colour;
+        }
+        
+        public string Name()
+        {
+            return _name;
+        }
+
+
     }
 
-    public class Quadrilateral : Shape { }
+    public class Quadrilateral : Shape
+    {
+        private int _NumSides = 4;
 
-    public class Square { }
+        public Quadrilateral(string Name, string Colour) : base (Name, Colour)
+        {
 
-    public class Rectangle { }
+        }
 
-    public class Circle {
+        public int NumSides()
+        {
+            return _NumSides;
+        }
+    }
 
+    public class Square : Quadrilateral
+    {
+        private double _sideLength;
+
+        public Square(string Name, string Colour, double sideLength) : base (Name, Colour)
+        {
+
+        }
+
+        public double GetArea()
+        {
+            return 0;
+        }
+
+        public double GetPerimeter()
+        {
+            return 0;
+        }
+    }
+
+    public class Rectangle : Quadrilateral
+    {
+        private double _length;
+        private double _width;
+
+        public Rectangle(string Name, string Colour, double Length, double Width) : base(Name, Colour)
+        {
+
+        }
+
+        public double GetArea()
+        {
+            return _length * _width;
+        }
+
+        public double GetPerimeter()
+        {
+            return 2 * _length + 2 * _width;
+        }
+    }
+
+    public class Circle : Shape
+    {
+        private double _radius;
         public const double PI = 3.142;
+
+        public Circle(string Name, string Colour, double Radius) : base (Name, Colour)
+        {
+
+        }
+
+        public double Radius()
+        {
+            return _radius;
+        }
+
+        public double GetArea()
+        {
+            return _radius * _radius * PI;
+        }
+
+        public double GetCircumference()
+        {
+            return 2 * _radius * PI;
+        }
+
+        public double GetPerimeter()
+        {
+            return 2 * _radius * PI;
+        }
     }
 }
